0911020074d90f75eed77f0e76fd92bf425efc7f
unknown <eyvaz.aliyev.7740@gmail.com>
2024-08-27 20:27:45 +0400

    Designed

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from './Card'
 
 function CardContainer({data}) {
+
+    const [text, setText] = useState('')
+
+    // const text = ''
+
+    console.log(text);
+
     return (
         <div>
+            <input value={text} onChange={(e)=> setText(e.target.value)}/>
+            <button onClick={() => setText('')}>Search</button>
             <Card name={data.name} age={data.age}/>
         </div>
     )
